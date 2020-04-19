e041157805d1fc7c5323c1dfcf3079a3af98e158
Xriamer Pang <Xriamer@outlook.com>
2020-04-20 01:11:44 +0800

    Add table shopcar and ShopcarDAOImpl

---
+++ out/artifacts/BookStore_war_exploded/js/shopcart.js
@@ -0,0 +1,42 @@
+var allPrice = 0.0;
+window.onload = function () {
+    document.getElementById("result").innerHTML = "<font class='red'>总价格:" + allPrice;
+}
+
+function calBooks(bid) {
+    var price = parseFloat(document.getElementById("price-" + bid).innerHTML);
+    var count = parseInt(document.getElementById(bid).value);
+    allPrice += (price * count);
+   // alert("bid=" + bid + ",price=" + price + ",count=" + count);
+    document.getElementById("cal-" + bid).innerHTML = "<font class='red'>" + (price * count);
+    if (document.getElementById("result") != undefined) {
+        document.getElementById("result").innerHTML = "<font class='red'>总价格:" + allPrice;
+    }
+
+}
+
+function addBut(bid) {
+    var price = parseFloat(document.getElementById("price-" + bid).innerHTML);
+    var count = parseInt(document.getElementById(bid).value);
+    allPrice -= (price * count);
+    var count = parseInt(document.getElementById(bid).value);
+    count++;
+    document.getElementById(bid).value = count;
+    calBooks(bid);
+}
+
+function subBut(bid) {
+    var price = parseFloat(document.getElementById("price-" + bid).innerHTML);
+    var count = parseInt(document.getElementById(bid).value);
+    allPrice -= (price * count);
+    var count = parseInt(document.getElementById(bid).value);
+    if (count >= 0) {
+        count--;
+        document.getElementById(bid).value = count;
+    }
+    calBooks(bid);
+}
+
+function calResult() {
+
+}
